271e018fecbc97dd007f7a595a3504c958e550b1
TipsyBoy <gudah13@naver.com>
2023-07-12 14:18:59 +0900

    Initial commit

---
+++ src/main/java/study/tipsyboy/loginApiJwt/LoginApiJwtApplication.java
@@ -0,0 +1,13 @@
+package study.tipsyboy.loginApiJwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginApiJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginApiJwtApplication.class, args);
+	}
+
+}
