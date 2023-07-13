b118bc135e41889f96b36732af2eda07f8902251
TipsyBoy <gudah13@naver.com>
2023-07-13 15:03:52 +0900

    Security에 대한 설정

---
+++ src/main/java/study/tipsyboy/loginApiJwt/security/SecurityConfig.java
@@ -0,0 +1,60 @@
+package study.tipsyboy.loginApiJwt.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import study.tipsyboy.loginApiJwt.jwt.JwtAccessDeniedHandler;
+import study.tipsyboy.loginApiJwt.jwt.JwtAuthenticationEntryPoint;
+import study.tipsyboy.loginApiJwt.jwt.config.JwtSecurityConfig;
+
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfig {
+
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtSecurityConfig jwtSecurityConfig;
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .antMatchers("/h2-console/**", "/favicon.ico");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().disable()
+
+                // 스프링 시큐리티의 기본 방식인 Session 방식을 사용하지 않기 위해서 Stateless 설정
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                // H2-Console 설정
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                // exception handling 을 위한 설정
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                // customFilter 를 기존 시큐리티 필터보다 앞에 설정했던 jwtSecurityConfig 를 적용
+                .and()
+                .apply(jwtSecurityConfig)
+
+                .and()
+                .authorizeRequests()
+                .mvcMatchers("/api/auth/**").permitAll()
+                .mvcMatchers("/**").hasAnyRole("MEMBER", "ADMIN");
+
+        return httpSecurity.build();
+    }
+}
