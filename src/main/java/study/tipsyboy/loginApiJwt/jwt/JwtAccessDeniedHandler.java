b4f3ca392d8161e1e7143a5289dd44cfc8d12da3
TipsyBoy <gudah13@naver.com>
2023-07-13 14:44:27 +0900

    entrypoint와 deniedHandler 구현 - 패키지를 어디에다가 둬야할지..?

---
+++ src/main/java/study/tipsyboy/loginApiJwt/jwt/JwtAccessDeniedHandler.java
@@ -0,0 +1,21 @@
+package study.tipsyboy.loginApiJwt.jwt;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+
+
+    @Override
+    public void handle(HttpServletRequest httpServletRequest,
+                       HttpServletResponse httpServletResponse,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+        httpServletResponse.sendError(HttpServletResponse.SC_FORBIDDEN);
+    }
+}
---
+++ src/main/java/study/tipsyboy/loginApiJwt/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,20 @@
+package study.tipsyboy.loginApiJwt.jwt;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest httpServletRequest,
+                         HttpServletResponse httpServletResponse,
+                         AuthenticationException authException) throws IOException, ServletException {
+
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+}
