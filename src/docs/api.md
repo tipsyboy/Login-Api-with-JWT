b3883bc958deb60d63410dc8c409fe74f96b6851
TipsyBoy <gudah13@naver.com>
2023-07-18 20:48:51 +0900

    doc upload test

---
+++ src/docs/api.md
@@ -0,0 +1,16 @@
+# API 호출 테스트
+
+-----------------------------------------
+
+## signup
+### request
+```HTTP
+POST /api/auth/signup
+Content-Type: application/json
+```
+```json
+{
+  "memberName": "tester",
+  "password": "tester"
+}
+```
--- src/main/java/study/tipsyboy/loginApiJwt/controller/member/MemberController.java
+++ src/main/java/study/tipsyboy/loginApiJwt/controller/member/MemberController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
