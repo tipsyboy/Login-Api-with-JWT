19aa8dc0abda64e9a8c2c1dda8b44410680abc2e
TipsyBoy <gudah13@naver.com>
2023-07-24 18:47:52 +0900

    signupForm 프론트 간단 구현

--- src/main/java/study/tipsyboy/loginApiJwt/controller/auth/AuthViewController.java
+++ src/main/java/study/tipsyboy/loginApiJwt/controller/auth/AuthViewController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import study.tipsyboy.loginApiJwt.dto.auth.MemberLoginRequestDto;
+import study.tipsyboy.loginApiJwt.dto.auth.MemberSignupRequestDto;
 
 @RequestMapping("/auth")
 @Controller
@@ -13,6 +14,11 @@ public class AuthViewController {
     @GetMapping("/login")
     public String loginForm(@ModelAttribute MemberLoginRequestDto memberLoginRequestDto) {
         return "auth/loginForm";
+    }
+
+    @GetMapping("/signup")
+    public String signupForm(@ModelAttribute MemberSignupRequestDto memberSignupRequestDto) {
+        return "auth/signupForm";
     }
 
 }
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,3 +1,21 @@
+// signupForm이 submit되었을 때
+$("#signupForm").submit(function (event) {
+    // 기본 동작 (페이지 새로고침)을 막음
+    event.preventDefault();
+
+    // 사용자가 입력한 값을 가져옴
+    var username = $("#username").val();
+    var password = $("#password").val();
+
+    // 서버에 보낼 데이터
+    var data = {
+        "memberName": username,
+        "password": password
+    };
+
+    sendAjaxRequest("POST", "/api/auth/signup", data, handleSignupSuccess, handleSimpleError);
+});
+
 // loginForm이 submit되었을 때
 $("#loginForm").submit(function (event) {
     // 기본 동작 (페이지 새로고침)을 막음
@@ -31,10 +49,18 @@ function sendAjaxRequest(type, url, data, successCallback, errorCallback) {
 
 // 로그인 성공시 처리
 function handleLoginSuccess(response) {
-    $("#result").text("Login Successful! Token: " + response.accessToken);
+    localStorage.setItem('accessToken', response.accessToken);
+    localStorage.setItem('refreshToken', response.refreshToken);
+    $("#result").text("Login Successful!");
 }
 
-// 로그인 실패시 처리
-function handleLoginError(error) {
+
+// 에러 간단하게 뿜기
+function handleSimpleError(error) {
     $("#result").text("Login Failed. Error: " + error.responseText);
+}
+
+// signup Success
+function handleSignupSuccess(response) {
+    $("#result").text("Signup Successful!");
 }
